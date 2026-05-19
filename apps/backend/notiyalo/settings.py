d5e2f18cbfcd9c28e2c16a78675d956f8bfcc9e0
Abhyudai-ajax <tiwaripreeti7586@gmail.com>
2026-05-20 01:03:30 +0530

    fix: extend JWT token lifetime to 30 days

--- apps/backend/notiyalo/settings.py
+++ apps/backend/notiyalo/settings.py
@@ -105,3 +105,9 @@ REST_FRAMEWORK = {
 }
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+from datetime import timedelta
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=30),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=90),
+}
